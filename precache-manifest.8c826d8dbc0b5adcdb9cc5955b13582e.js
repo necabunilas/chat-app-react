bdcc0c2d2ca81fb8a1dcf3ef382eb3f52a6f4a5d
Nicole Eric Cabunilas <necabunilas@gmail.com>
2020-07-01 12:17:04 +0800

    Updates

---
+++ precache-manifest.8c826d8dbc0b5adcdb9cc5955b13582e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe1300b2ae8b4532cabad190b67d965",
+    "url": "/chat-app-react/index.html"
+  },
+  {
+    "revision": "38186dbefb9155cd480f",
+    "url": "/chat-app-react/static/css/main.18c6ca55.chunk.css"
+  },
+  {
+    "revision": "d31894ca4d20e980e22a",
+    "url": "/chat-app-react/static/js/2.a7512507.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/chat-app-react/static/js/2.a7512507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38186dbefb9155cd480f",
+    "url": "/chat-app-react/static/js/main.68278284.chunk.js"
+  },
+  {
+    "revision": "874e71487e2bb66f5698",
+    "url": "/chat-app-react/static/js/runtime-main.5ea667bd.js"
+  }
+]);
